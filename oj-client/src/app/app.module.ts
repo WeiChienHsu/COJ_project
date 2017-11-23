0509449fa8bc03c2a1d610466c775f2d758ddf2c
WeiChien Hsu <weichien711@gmail.com>
2017-11-23 00:54:14 -0600

    :construction: add Routes

--- oj-client/src/app/app.module.ts
+++ oj-client/src/app/app.module.ts
@@ -1,20 +1,26 @@
+import { routing } from './app.routes';
+import { DataService } from './services/data.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProblemListComponent
+    ProblemListComponent,
+    ProblemDetailComponent
   ],
   imports: [
     BrowserModule
+    routing
   ],
-  providers: [],
+  providers: [
+    DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
