c3462683e818afd2e7ab3877de6a919961fd05d2
WeiChien Hsu <weichien711@gmail.com>
2017-12-03 22:41:38 -0600

    :bug: Typo: import ace.js

--- oj-client/src/app/components/editor/editor.component.ts
+++ oj-client/src/app/components/editor/editor.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const ace: any;
+
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements OnInit {
-
+  editor: any;
+  defaultContent = {
+    'Java': `public class Example {
+      public static void main(String[] args) {
+          // Type your Java code here
+      }
+    }`,
+    'Python': `class Solution:
+    def example():
+        # Write your Python code here`
+   };
   constructor() { }
 
   ngOnInit() {
+    this.editor = ace.edit("editor");
+    this.editor.setTheme("ace/theme/eclipse");
+    this.editor.getSession().setMode("ace/mode/java");
+    this.editor.setValue(this.defaultContent['Java'])
   }
 
+
+  private newMethod(): any {
+    return "ace/theme/eclipse";
+  }
 }
