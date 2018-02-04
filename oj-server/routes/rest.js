5fb2b79e529c7001adbc7949e92451b69c38d4e0
WeiChien Hsu <weichien711@gmail.com>
2018-02-03 21:48:16 -0600

    :bug: fix comments

--- oj-server/routes/rest.js
+++ oj-server/routes/rest.js
@@ -10,13 +10,13 @@ EXECUTOR_SERVER_URL = 'http://localhost:5000/build_and_run';
 
 restClient.registerMethod('build_and_run', EXECUTOR_SERVER_URL, 'POST');
 
-// get problems
+// get all problems
 router.get('/problems', (req, res) => {
     problemService.getProblems()
         .then(problems => res.json(problems));
 });
 
-// get problems
+// get a single problem
 router.get('/problems/:id', (req, res) => {
     const id = req.params.id;
     problemService.getProblem(+id)
